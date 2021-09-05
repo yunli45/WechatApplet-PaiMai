037df7f87972ebadd8466763aa8b229a2a697e24
yunli45 <yunli_45@163.com>
2021-09-05 23:11:20 +0800

    自动获取详情页的标题，详情页新增了拍卖品状态信息

--- pages/DetailPage/DetailPage.js
+++ pages/DetailPage/DetailPage.js
@@ -16,7 +16,9 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        
         this.data.id = options.id;
+
         for (let index = 0; index < this.data.list.length; index++) {
             if (this.data.list[index].id == this.data.id) {
                 this.setData({
@@ -25,30 +27,18 @@ Page({
                 })
             }
 
-        }
-        console.log(this.data.background)
+        };
+        wx.setNavigationBarTitle({
+            title: this.data.info.title
+        })
+        
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-       /* if (this.data.info.img) {
-            let curImgList = this.data.info.img; // 当前图片的数组
-            let curIndex = 0; // 设置一个局部变量，当前的图片下标
-            let myImg = document.getElementById("img")
-            document.getElementById("bigImg");
-            // 设置一个时间，进行切换
-            console.log("正在循环照片1")
-            setTimeout(function () {
-                for (let i = 0; i < curImgList.length; i++) {
-                    myImg.src = curImgList[i];
-                    console.log("正在循环照片2")
-                }
-            }, 2000)
-            console.log("正在循环照片3")
-        }*/
-
+     
     },
 
     /**
